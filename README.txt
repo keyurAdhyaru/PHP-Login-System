c21c6a113b7a4c0a96609e895c1f13421dbb21fb
Keyur Adhyaru <48583666+keyurAdhyaru@users.noreply.github.com>
2020-05-12 16:06:58 +0530

    login system

---
+++ README.txt
@@ -0,0 +1,18 @@
+>>> PHP	LOGIN SYSTEM
+
+Features
+
+#Register user (register.php)
+#login user (login.php)
+#config the database(config.php)
+#reset password (reset-password.php)
+#welcome screen(welcome.php)
+#encrypted password  
+#validation
+
+How to run this system.
+
+#Copy the folder to you server.
+#import the sql file.
+#run login.php 
+
---
+++ details.sql
@@ -0,0 +1,51 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.5
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3306
+-- Generation Time: May 12, 2020 at 10:31 AM
+-- Server version: 5.7.26
+-- PHP Version: 7.2.18
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `users`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `details`
+--
+
+DROP TABLE IF EXISTS `details`;
+CREATE TABLE IF NOT EXISTS `details` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `created_at` datetime DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `username` (`username`)
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `details`
+--
+
+INSERT INTO `details` (`id`, `username`, `password`, `created_at`) VALUES
+(1, 'dark', '$2y$10$Nz6fYTIL4XIuK8bzA0jxcesdsZfAD46NaSwylHWXyuRgdh3aYK4qG', '2020-05-12 15:49:43');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
